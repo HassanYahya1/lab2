81d119bb7550a9830bcaa47a761162e7a387b083
hasaan yahya <h.2011.h@hotmail.com>
2021-09-28 21:02:10 +0300

    finesh

---
+++ Desktop/week1-day2-lab1/MyPlayground1.playground/Contents.swift
@@ -0,0 +1,43 @@
+import UIKit
+
+// 1. Create two integer variables a and b
+var a = 10
+var v = 15
+
+// 2.Print the sum of these variable
+print(a+v)
+
+// 3.Create array of 6 elements
+var names = ["hassan",
+               "ali",
+               "sara",
+               "saeed",
+               "abdulaziz",
+               "hadi"]
+
+// 4. Add the value 7 to the array
+names.append("mram")
+
+// 5.Print the fourth element of the array
+print(names[4])
+
+
+// 6. Delete the second element of the array
+names.remove(at: 2)
+
+
+// 7. Create a set of 4 elements
+var numbers = Set ([9, 8, 11, 5])
+
+
+// 8.Print every element in the set
+print(numbers)
+
+
+// 9.Create a string variable that holds your name
+var myName = "Hasaan"
+
+
+// 10.Print your name (the variable)
+print(myName)
+
